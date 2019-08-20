24c6ca1fdd4b88010919ecdaf536620c242c615b
cvjso <50202603+cvjso@users.noreply.github.com>
2019-08-20 14:50:31 +0000

    pass item/item in matriz

--- matriz.c
+++ matriz.c
@@ -3,11 +3,11 @@
 
 int main(){
 
-	int matriz01[][];
-	int matriz02[][];
-	int matriz03[][];
-	int a = 0;
-	int b = 0;
+	int matriz01[3][];
+	int matriz02[3][];
+	int matriz03[3][];
+	int d = 0;
+	int c = 0;
 
 	printf("Digite os numeros da primeira matriz 3x3 ");
 	scanf("%d",matriz01);
@@ -18,21 +18,15 @@ int main(){
 	for (d=0; d<3; d++){
 	
 		for(c=0;c<3;c++){
-			matriz03[d][c] = matriz01[a][0]*matriz02[0][b]+
-				matriz01[a][1]*matriz02[1][b]+
-				matriz01[a][2]*matriz02[2][b];
-		
-		
-		
+			matriz03[d][c] = matriz01[d][0]*matriz02[0][c] + 
+matriz01[d][1]*matriz02[1][c] + matriz01[d][2]*matriz02[2][c];
 		}
 	
-	
 	}
-
-
-
-
-
+	printf("A matriz resultante foi\n");
+	printf("%d %d %d",matriz03[0][0],matriz03[0][1],matriz03[0][2]);
+	printf("%d %d %d",matriz03[0][0],matriz03[1][1],matriz03[1][2]);
+	printf("%d %d %d",matriz03[0][0],matriz03[2][1],matriz03[2][2]);
 
 	return 0;
 }
