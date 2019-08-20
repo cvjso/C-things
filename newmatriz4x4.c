08eca14a4c48232e6935b7bd123b50ab8361cace
cvjso <50202603+cvjso@users.noreply.github.com>
2019-08-20 19:40:08 +0000

    Add files via upload

---
+++ newmatriz4x4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+	int matriz01[4][4];
+	int a,b,l;
+	printf("Digite os numeros da matriz 4x4\n");
+	for (i=0;i<4;i++){
+		for(j;j<4;j++){
+			scanf("%d",&matriz01[i][j]);
+
+		}
+	}
+	
+
+
+
+
+
+	return 0;
+}
